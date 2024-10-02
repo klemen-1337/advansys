37e66b90ada89c251da89db6ba6710cf8c6ef541
Klemen <kklemen.stefe@gmail.com>
2024-10-02 14:33:30 +0200

    Added transclude for user details

--- app/userInfoComponent.js
+++ app/userInfoComponent.js
@@ -3,46 +3,9 @@ app.component('userInfo', {
         user: '<',
         onDelete: '&'
     },
-    template: `
-        <div class="user-info">
-            <h3>User details</h3>
-            <table>
-                <tr>
-                    <td>Username</td>
-                    <td>Name</td>
-                    <td>Email</td>
-                </tr>
-                <tr>
-                    <td><strong>{{$ctrl.user.username}}</strong></td>
-                    <td><strong>{{$ctrl.user.name}}</strong></td>
-                    <td><strong>{{$ctrl.user.email}}</strong></td>
-                </tr>
-                <tr>
-                    <td>Website</td> 
-                    <td>Phone number</td>
-                    
-                </tr>
-                <tr>
-                    <td><strong>{{$ctrl.user.website}}</strong></td>
-                    <td><strong>{{$ctrl.user.phone}}</strong></td>
-                </tr>
-            </table>
-            <h5>Address</h5>
-            <div class="address">
-                <p><strong>Street: {{$ctrl.user.address.street}}</strong></p>  
-                <p><strong>Suite: {{$ctrl.user.address.suite}}</strong></p>
-                <p><strong>City: {{$ctrl.user.address.city}}</strong></p>
-                <p><strong>Zip Code: {{$ctrl.user.address.zipcode}}</strong></p>  
-            <div>   
-            <p style="text-align: right" >
-                <button class="btn btn-outline-danger" ng-click="$ctrl.deleteUser(user.id, $event)">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"></path>
-                    </svg>
-                </button>
-            <p>
-        </div>
-    `,
+    transclude: true,
+    templateUrl: 'user-info.template.html',
+
     controller: function() {
         var ctrl = this;
     
